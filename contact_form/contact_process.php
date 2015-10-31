20a06a2ec12960a0e4a865bc667c9460169d9089
AnakinFoxe <xing.y.hu@gmail.com>
2015-10-31 15:06:53 -0700

    Small modification for Contact so the subject will be shown in the email.

--- contact_form/contact_process.php
+++ contact_form/contact_process.php
@@ -1,6 +1,9 @@
 <?php
 
+$subject = $_POST['subject'];
 $name = $_POST['name'];
+$mail_subject = "[Contact] " . $name . ": " . $subject;
+
 $message = $_POST['message'];
 $from = $_POST['email'];
 $headers = "From:" . $from;
@@ -9,7 +12,7 @@ if (empty($name) || empty($message) || empty($from)) {
     echo "Nothing should happen...";
 } else {
     $to = "xing.y.hu@gmail.com";
-    mail($to,$name,$message,$headers);
+    mail($to,$mail_subject,$message,$headers);
     echo "Mail Sent.";
 }
 
